e1848f1559043e92be29a0bf0f661bdd736ab056
IchBinAniket <aniketbali777@gmail.com>
2025-12-17 16:37:02 +0530

    configured efcore

---
+++ Sutradhaar.API/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sutradhaar.API.Models;
+
+namespace Sutradhaar.API.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options) { }
+
+        public DbSet<Enquiry> Enquiries { get; set; }
+        public DbSet<Package> Packages { get; set; }
+        public DbSet<TicketBooking> TicketBookings { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Vehicle> Vehicles { get; set; }
+
+    }
+}
---
+++ Sutradhaar.API/Data/AppDbContextFactory.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace Sutradhaar.API.Data
+{
+    public class AppDbContextFactory
+        : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            // Build configuration
+            IConfiguration configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+
+            optionsBuilder.UseSqlServer(
+                configuration.GetConnectionString("DefaultConnection"));
+
+            return new AppDbContext(optionsBuilder.Options);
+        }
+    }
+}
